e2b6f64399251b700c70274cf95b6bb6b8ec55cf
ookamiyoujyo <ookamiyoujyo@gmail.com>
2021-06-17 12:16:35 +0900

    増やす

--- v0617/Form1.Designer.cs
+++ v0617/Form1.Designer.cs
@@ -35,14 +35,17 @@ namespace v0617
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(278, 207);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(255, 195);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(67, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -81,17 +84,43 @@ namespace v0617
             this.label4.TabIndex = 3;
             this.label4.Text = "label4";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(121, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(235, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +133,8 @@ namespace v0617
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -13,11 +13,19 @@ namespace v0617
     public partial class Form1 : Form
     {
         int score = 100;
-        int vx = -10;
-        int vy = -10;
+        int vx = rand.Next(-10, 11);
+        int vy = rand.Next(-10, 11);
+        int vx1 = rand.Next(-10, 11);
+        int vy1 = rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
+
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            label1.Left = rand.Next() % ClientSize.Width;
+            label1.Top = rand.Next() % ClientSize.Height;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -31,6 +39,13 @@ namespace v0617
 
             label1.Left += vx;
             label1.Top += vy;
+
+            if (    (fpos.Y >= label1.Top)
+                 && (fpos.Y < label1.Bottom)
+                 && (fpos.X <= label1.Left)
+                 && (fpos.X > label1.Right)
+                )
+                timer1.Enabled = false;
 
             if (label1.Left < 0)
                 vx = vx * -1;
@@ -52,14 +67,30 @@ namespace v0617
                 timer1.Enabled = false;
             }
 
-            if (   (fpos.Y >= label1.Top) 
-                && (fpos.Y < label1.Bottom) 
-                && (fpos.X <= label1.Left)
-                && (fpos.X > label1.Right)
-                )
-            {
-                timer1.Enabled = false;
-            }
+
+            label5.Left += vx1;
+            label5.Top += vy1;
+
+            if (label5.Left < 0)
+                vx1 = vx1 * -1;
+            if (label5.Top < 0)
+                vy1 = -vy1;
+            if (label5.Right > ClientSize.Width)
+                vx1 = vx1 * -1;
+            if (label5.Bottom > ClientSize.Height)
+                vy1 = -vy1;
+
+            label6.Left += vx2;
+            label6.Top += vy2;
+
+            if (label6.Left < 0)
+                vx2 = vx2 * -1;
+            if (label6.Top < 0)
+                vy2 = -vy2;
+            if (label6.Right > ClientSize.Width)
+                vx2 = vx2 * -1;
+            if (label6.Bottom > ClientSize.Height)
+                vy2 = -vy2;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -68,6 +99,21 @@ namespace v0617
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
